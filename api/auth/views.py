f23c9c28260f0f4bf878af8e4c9a220c476fe136
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-05-30 23:41:49 +0500

    Add authentication and order namespaces with sample endpoints

--- api/auth/views.py
+++ api/auth/views.py
@@ -2,7 +2,21 @@ from flask_restx import Namespace, Resource, fields
 
 auth_namespace = Namespace('auth', description='Authentication related operations')
 
-@auth_namespace.route('/')
-class HomeAuth(Resource):
-    def get(self):
-        return {'message': 'Hellow Mohsin ! '}, 200 
+@auth_namespace.route('/signup')
+class SignUp(Resource):
+
+    def post(self):
+        """
+            Sign up a new user.
+        """
+        pass
+
+
+@auth_namespace.route('/login')
+class Login(Resource):
+    
+    def post(self):
+        """
+            Log in an existing user.
+        """
+        pass
--- api/orders/views.py
+++ api/orders/views.py
@@ -3,7 +3,29 @@ from flask_restx import Namespace, Resource, fields
 orders_namespace = Namespace('orders', description='Order related operations')
 
 
-@orders_namespace.route('/')
-class HomeOrders(Resource):
+@orders_namespace.route('/orders')
+class OrderGetCreate(Resource):
+
     def get(self):
-        return {'message': 'Hello from Orders!'}, 200
+        """
+            Get all orders.
+        """
+        pass
+
+    def post(self):
+        """
+            Create a new order.
+        """
+        pass
+
+@orders_namespace.route('/orders/<int:order_id>')
+class OrderGetUpdateDelete(Resource):
+
+    def get(self, order_id):
+        pass
+
+    def put(self, order_id):
+        pass
+
+    def delete(self, order_id):
+        pass
