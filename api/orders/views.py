fc46b5d6f28a74db7da1f6094e8c0da643f500cc
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-05-31 16:43:44 +0500

    Add order-related resource routes to the API

--- api/orders/views.py
+++ api/orders/views.py
@@ -18,14 +18,50 @@ class OrderGetCreate(Resource):
         """
         pass
 
-@orders_namespace.route('/orders/<int:order_id>')
-class OrderGetUpdateDelete(Resource):
+@orders_namespace.route('/order/<int:order_id>')
+class GetUpdateDelete(Resource):
 
     def get(self, order_id):
+        """
+            Get an order by ID.
+        """
         pass
 
     def put(self, order_id):
+        """
+            Update an order by ID.
+        """	
         pass
 
     def delete(self, order_id):
+        """
+            Delete an order by ID.
+        """
         pass
+
+@orders_namespace.route('/user/<int:user_id>/order/<int:order_id>')
+class GetSpecificUserOrder(Resource):
+
+    def get(self, user_id, order_id):
+        """
+            Get an order by user ID and order ID.
+        """
+        pass
+
+@orders_namespace.route('/user/<int:user_id>/orders')
+class UserOrders(Resource):
+
+    def get(self, user_id):
+        """
+            Get all orders for a specific user.
+        """
+        pass
+
+@orders_namespace.route('/order/status/<int:order_id>')
+class UpdateOrderStatus(Resource):
+    def patch(self, order_id):
+        """
+            Update the status of an order by ID.
+        """
+        pass
+    
