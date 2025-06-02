4695a2742fa27d8ff46e0e8afa866d2183f34ff8
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-02 21:26:59 +0500

    Enhance application setup with database integration and shell context support

--- api/config/config.py
+++ api/config/config.py
@@ -1,14 +1,15 @@
 import os
 from decouple import config
 
+BASR_DIR = os.path.dirname(os.path.realpath(__file__))
 class Config:
    SECRET_KEY = config('SECRET_KEY', 'secret')
-
+   SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
-    DEBUG = config('DEBUG', default=True, cast=bool)
-    SQLALCHEMY_DATABASE_URI = config('DEV_DATABASE_URL', 'sqlite:///dev.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DEBUG = config('DEBUG', cast=bool)
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASR_DIR, 'db.sqlite3')
 
 class TestConfig(Config):
     pass
