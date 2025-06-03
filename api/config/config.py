a39b6f587b9511517005ddc0d332545d3a9c5080
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-04 01:25:35 +0500

    Add PyJWT and PyJWS implementations with support for JWK and JWK Set caching
    - Introduced `api_jwt.py` and `api_jws.py` for JWT and JWS handling.
    - Implemented exception handling in `exceptions.py` for various JWT errors.
    - Added utility functions in `utils.py` for encoding/decoding and PEM format checks.
    - Created `help.py` for generating bug report information.
    - Implemented `jwk_set_cache.py` for caching JWK sets with expiration.
    - Developed `jwks_client.py` for fetching and managing JWK sets from a URI.
    - Added type definitions in `types.py` for better type hinting.
    - Introduced warning management in `warnings.py` for deprecated features.
    - Ensured compatibility with cryptography library for signature handling.

--- api/config/config.py
+++ api/config/config.py
@@ -1,15 +1,19 @@
 import os
 from decouple import config
+from datetime import timedelta
 
 BASR_DIR = os.path.dirname(os.path.realpath(__file__))
 class Config:
    SECRET_KEY = config('SECRET_KEY', 'secret')
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
+   JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+   JWT_REFRESH_TOKEN_EXPIRES = timedelta(minutes=30)
+   JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 
 class DevConfig(Config):
-    DEBUG = config('DEBUG', cast=bool)
-    SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASR_DIR, 'db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = True
+    DEBUG = True
 
 class TestConfig(Config):
     pass
