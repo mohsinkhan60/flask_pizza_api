9107552cc6afbbbbbd11fb092677ce9b07009f62
mohsinkhan60 <mohsinkhan.contact0@gmail.com>
2025-06-06 23:59:57 +0500

    Fix order of username and email column definitions in User model

--- api/models/users.py
+++ api/models/users.py
@@ -4,8 +4,8 @@ class User(db.Model):
    __tablename__ = 'users'
 
    id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(80), nullable=False)
    email = db.Column(db.String(120), unique=True, nullable=False)
+   username = db.Column(db.String(80), nullable=False)
    password_hash = db.Column(db.String(200), nullable=False)
    is_staff = db.Column(db.Boolean(), default=False)
    is_active = db.Column(db.Boolean(), default=True)
